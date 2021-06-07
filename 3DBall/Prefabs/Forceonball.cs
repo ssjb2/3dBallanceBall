7d7862f9301f45570b3cfac3687def19889b2347
ssjb2 <36816150+ssjb2@users.noreply.github.com>
2021-06-07 15:34:11 +0200

    Add files via upload

---
+++ 3DBall/Prefabs/Forceonball.cs
@@ -0,0 +1,17 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ 
+ public class Forceonball : MonoBehaviour {
+ 
+     public GameObject windObject;
+     Rigidbody rigidbody;
+ 
+     void Start () {
+         rigidbody = gameObject.GetComponent<Rigidbody> ();
+     }
+ 
+     void FixedUpdate () {
+         rigidbody.AddForce (windObject.transform.forward * 10);
+     }
+ }
+ 
---
+++ 3DBall/Prefabs/WindDirection.cs
@@ -0,0 +1,11 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ 
+ public class WindDirection : MonoBehaviour {
+ 
+     void Start () {
+         new WaitForSeconds(1);
+         transform.rotation = Random.rotation;
+     }
+ }
+ 
